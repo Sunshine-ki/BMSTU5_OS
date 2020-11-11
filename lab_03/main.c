038785020f3c845d8ed287486e0d0a34f94ddc0c
Sunshine-ki <sukocheva.alis@mail.ru>
2020-11-11 17:08:32 +0300

    Сдано

--- lab_03/main.c
+++ lab_03/main.c
@@ -5,21 +5,26 @@ int main()
 {
 	int childpid;
 
+	// Создается дочерний процесс
+	// Тут содается 2 идентичных процесса.
 	if ((childpid = fork()) == -1)
 	{
+		// Если при порождении процесса произошла ошибка.
 		perror("Can\'t fork.\n");
 		return 1;
 	}
 	else if (childpid == 0)
 	{
+		// Это процесс потом (ребенок).
 		while (1)
-			printf("%d ", getpid());
+			printf("Baby: %d\n", getpid());
 		return 0;
 	}
 	else
 	{
+		// Это процесс предок.
 		while (1)
-			printf("%d ", getpid());
+			printf("Daddy: %d\n", getpid());
 		return 0;
 	}
 }
