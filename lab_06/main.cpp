81ed537dc0f6c1cdbeb3d3ff8156fe1b2400a2df
Sunshine-ki <sukocheva.alis@mail.ru>
2020-12-22 09:51:02 +0300

    Update

--- lab_06/main.cpp
+++ lab_06/main.cpp
@@ -8,35 +8,44 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define OK 0
+
+#define CREATE_MUTEX_ERROR 1
+#define CREATE_EVENT_ERROR 2
+#define CREATE_READER_THREAD_ERROR 3
+#define CREATE_WRITER_THREAD_ERROR 3
 
 #define READERS_NUMBER 3
 #define WRITERS_NUMBER 3
 
 #define ITERATIONS_NUMBER 10
 
-HANDLE canRead; // event.
+HANDLE canRead;	 // event.
+HANDLE canWrite; // event.
+HANDLE mutex;
 
-HANDLE reader_Threads[READERS_NUMBER];
-HANDLE writer_Threads[WRITERS_NUMBER];
+LONG waitingWritersCount = 0;
+LONG waitingReadersCount = 0;
+LONG activeReadersCount = 0;
+bool writing = false;
 
-HANDLE hMutex;
+HANDLE readerThreads[READERS_NUMBER];
+HANDLE writerThreads[WRITERS_NUMBER];
 
 int readersID[READERS_NUMBER];
 int writersID[WRITERS_NUMBER];
 
 // int readersRand[READERS_NUMBER * ITERATIONS_NUMBER];
 
-// volatile LONG active_readers = 0, waiting_writers = 0;
-
 int value = 0;
 
-// void StartRead()
-// {
-// }
+void StartRead()
+{
+}
 
-DWORD WINAPI Writer(CONST LPVOID param)
+void StopRead()
 {
 }
 
@@ -45,14 +54,47 @@ DWORD WINAPI Reader(CONST LPVOID param)
 	int id = *(int *)param;
 	for (int i = 0; i < ITERATIONS_NUMBER; i++)
 	{
-		WaitForSingleObject(canRead, INFINITE);
+		// StartRead();
+		// printf("Reader with id = %d, i = %d value = %d\n", id, i, value);
+		// StopRead();
 
+		WaitForSingleObject(canRead, INFINITE);
 		printf("Thread with id = %d, i = %d value = %d\n", id, i, value);
 		Sleep(20);
 	}
 }
 
-void CreateThreads()
+DWORD WINAPI Writer(CONST LPVOID param)
+{
+}
+
+int InitHandles()
+{
+	// 2ой аргумент == false значит мьютекс свободный.
+	if ((mutex = CreateMutex(NULL, FALSE, NULL)) == NULL)
+	{
+		perror("CreateMutex");
+		return CREATE_MUTEX_ERROR;
+	}
+
+	// 2ой аргумент == true значит сброс вручную
+	// 3ий аргумент == true значит объект в сигнальном состоянии.
+	if ((canRead = CreateEvent(NULL, TRUE, TRUE, NULL)) == NULL)
+	{
+		perror("CreateEvent (canRead)");
+		return CREATE_EVENT_ERROR;
+	}
+
+	if ((canWrite = CreateEvent(NULL, TRUE, TRUE, NULL)) == NULL)
+	{
+		perror("CreateEvent (canWrite)");
+		return CREATE_EVENT_ERROR;
+	}
+
+	return OK;
+}
+
+int CreateThreads()
 {
 	DWORD id = 0;
 	for (int i = 0; i < READERS_NUMBER; i++)
@@ -65,29 +107,40 @@ void CreateThreads()
 		// readersID + i - указатель на переменную, которая передается в поток;
 		//  0 - исполнение потока начинается немедленно;
 		// Последний - адрес переменной типа DWORD, в которую функция возвращает идентификатор потока.
-		reader_Threads[i] = CreateThread(NULL, 0, &Reader, readersID + i, 0, &id);
+		if ((readerThreads[i] = CreateThread(NULL, 0, &Reader, readersID + i, 0, &id)) == NULL)
+		{
+			perror("CreateThread (reader)");
+			return CREATE_READER_THREAD_ERROR;
+		}
 		printf("Created reader with thread id = %d\n", id);
 	}
 
 	for (int i = 0; i < WRITERS_NUMBER; i++)
 	{
 		writersID[i] = i;
-		writer_Threads[i] = CreateThread(NULL, 0, &Writer, writersID + i, 0, &id);
+		if ((writerThreads[i] = CreateThread(NULL, 0, &Writer, writersID + i, 0, &id)) == NULL)
+		{
+			perror("CreateThread (writer)");
+			return CREATE_WRITER_THREAD_ERROR;
+		}
 		printf("Created writer with thread id = %d\n", id);
 	}
+
+	return OK;
 }
 
 void Close()
 {
 	// Закрываем дескрипторы mutex, event и всех созданных потоков.
 	for (int i = 0; i < READERS_NUMBER; i++)
-		CloseHandle(reader_Threads[i]);
+		CloseHandle(readerThreads[i]);
 
 	for (int i = 0; i < WRITERS_NUMBER; i++)
-		CloseHandle(writer_Threads[i]);
+		CloseHandle(writerThreads[i]);
 
 	CloseHandle(canRead);
-	CloseHandle(hMutex);
+	CloseHandle(canWrite);
+	CloseHandle(mutex);
 }
 
 // void CreateRand()
@@ -99,26 +152,28 @@ void Close()
 int main(void)
 {
 	setbuf(stdout, NULL);
-	srand(time(NULL));
+	// srand(time(NULL));
+
+	int err;
 
 	printf("\n\n\n\n\n\n\n\n");
-	// 2ой аргумент == false значит мьютекс свободный.
-	hMutex = CreateMutex(NULL, FALSE, NULL); // CONST
-
-	// 2ой аргумент == true значит сброс вручную
-	// 3ий аргумент == true значит объект в сигнальном состоянии.
-	canRead = CreateEvent(NULL, TRUE, TRUE, NULL);
 
 	// CreateRand();
 
-	CreateThreads();
+	err = InitHandles();
+	if (err)
+		return err;
+
+	err = CreateThreads();
+	if (err)
+		return err;
 
 	// READERS_NUMBER - кол-во инетерсующих нас объектов ядра.
-	// reader_Threads - указатель на массив описателей объектов ядра.
+	// readerThreads - указатель на массив описателей объектов ядра.
 	// TRUE - функция не даст потоку возобновить свою работу, пока не освободятся все объекты.
 	// INFINITE - указывает, сколько времени поток готов ждать освобождения объекта.
-	WaitForMultipleObjects(READERS_NUMBER, reader_Threads, TRUE, INFINITE);
-	WaitForMultipleObjects(WRITERS_NUMBER, writer_Threads, TRUE, INFINITE);
+	WaitForMultipleObjects(READERS_NUMBER, readerThreads, TRUE, INFINITE);
+	WaitForMultipleObjects(WRITERS_NUMBER, writerThreads, TRUE, INFINITE);
 
 	printf("\n\n\n\n\n\n\n\n");
 
